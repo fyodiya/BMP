cbb9bc39c284b9a64b53a8be7a0594dd0aaf88ca
Elina <88678660+fyodiya@users.noreply.github.com>
2023-08-15 13:17:48 +0300

    Initial commit

---
+++ src/main/scala/BMP.scala
@@ -0,0 +1,29 @@
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.FileOutputStream
+import java.io.OutputStream
+
+object BMP extends App {
+
+  // Text to be converted to an image
+  val text = "HELLO"
+
+  // Create an SVG for monochrome image
+  val svgContent = s"""<svg xmlns="http://www.w3.org/2000/svg" width="64" height="16">
+                      |  <rect width="64" height="16" fill="white"/>
+                      |  <text x="4" y="12" font-family="Arial" font-size="12" fill="black">$text</text>
+                      |</svg>""".stripMargin
+
+  // Convert SVG to BMP using PNGTranscoder
+  val transcoder = new PNGTranscoder
+  val input = new TranscoderInput(new ByteArrayInputStream(svgContent.getBytes))
+  val outputStream = new ByteArrayOutputStream
+  val output = new TranscoderOutput(outputStream)
+  transcoder.transcode(input, output)
+  outputStream.close()
+
+  // Save the BMP image
+  val bmpFile = new FileOutputStream("monochrome_image.bmp")
+  bmpFile.write(outputStream.toByteArray)
+  bmpFile.close()
+}
